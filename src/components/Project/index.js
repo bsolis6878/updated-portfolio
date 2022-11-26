f2ae0caec1bbde204c186c7b7be690cd163d11c1
bsolis6878 <bsolis6878@gmail.com>
2022-11-26 08:06:17 -0600

    completed about me section

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,11 +2,18 @@ function Project({ currentlySelected }) {
     if (currentlySelected === 'About Me') {
         return (
             <section>
-                <div>
-
+                <div className='profile-picture'>
+                    <img src={require('../../assets/profile.JPG')} alt='Image of developer Bryan Solis' />
                 </div>
-                <div>
-                    
+                <div className='about-me'>
+                    <h3>Welcome to my portfolio!</h3>
+                    <p>
+                        As an aspiring web developer, I have hands on experience with many web development technologies including
+                        HTML, CSS, front-end and back-end Javascript, the MERN stack (MongoDB, Express.js, React, and Node.js),
+                        MySQL using Sequelize, Git and GitHub, third-party and server-side APIs, and the Model-View-Controller (MVC)
+                        structure. I believe my ability to grasp new concepts and apply them effectively would be a great asset to
+                        any project, so thank you in advance for your consideration!
+                    </p>
                 </div>
             </section>
         )
