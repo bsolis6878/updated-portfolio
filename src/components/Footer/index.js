0f5e04d54f256c3f684a9876dc3eada534da3e42
bsolis6878 <bsolis6878@gmail.com>
2022-11-26 13:55:20 -0600

    added footer icons with links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,15 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            
+            <a href='https://github.com/bsolis6878' target='_blank'>
+                <img src={require('../../assets/github.png')} alt='Github icon' />
+            </a>
+            <a href='https://www.linkedin.com/in/bryan-solis-54b859243/' target='_blank'>
+                <img src={require('../../assets/linkedin.png')} alt='Linkedin icon' />
+            </a>
+            <a href='https://twitter.com/bsolis6878' target='_blank'>
+                <img src={require('../../assets/twitter.png')} alt='Twitter icon' />
+            </a>
         </footer>
     )
 }
